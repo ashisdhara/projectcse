49bf5676041019a59a11e084f090cd9717c85ee5
ashis <ashishere15@gmail.com>
2015-04-03 01:22:05 +0530

    ashis : basic queries and comments working

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,89 +1,86 @@
 <?php
 
-class Home extends CI_Controller{
-    public function index(){
+class Home extends CI_Controller {
+
+    public function index() {
         session_start();
-        if(!isset($_SESSION['user_id']))
-        {
-        $this->login(); 
+        if (!isset($_SESSION['user_id'])) {
+            $this->login();
+        } else {
+            $this->user_homepage();
+        }
     }
-    else
-    {
-        $this->user_homepage();
+
+    public function login() {
+        $this->load->view('login_view');
     }
-        
-    }
-    public function login()
-	{
-		$this->load->view('login_view') ;
-	}
-    public function login_submit()
-    {    session_start();
+
+    public function login_submit() {
+        session_start();
         $this->load->model('users_model');
         $email = $this->input->post('email_id');
         $password = $this->input->post('password');
-        $result=$this->users_model->check_login($email,$password);
-        $user_id=$result['id'];
-        if($result==0)
-        {
+        $result = $this->users_model->check_login($email, $password);
+        $user_id = $result['id'];
+        if ($result == 0) {
             echo "error!";
-        }
-        else
-        {
-            $_SESSION['user_id']=$user_id;
+        } else {
+            $_SESSION['user_id'] = $user_id;
             echo $_SESSION['user_id'];
-            $url=base_url('/home');
+            $url = base_url('/home');
             header("Location:$url");
         }
     }
 
-     public function registration() {
-     $this->load->view("registration_view");}
-     
-     public function registration_submit()
-     {
-         session_start();
-         echo "inside register submit" ; 
-         $name = $this->input->post('name');
-         $email = $this->input->post('email');
-         $password = $this->input->post('password');
-         $username = $this->input->post('username');
-         $phonenumber = $this->input->post('phonenumber');
-         //echo "$name";
-         $this->load->model('users_model');
-         $this->users_model->register_insert($username , $email , $password , $name , $phonenumber);
-         $url="home/login";
-         header("Location:$url");
-     }
-     public function user_homepage()
-     {
-         $this->load->view("home_view");
-     }
-     
-     public function display_queries_and_comments()
-     { 
+    public function registration() {
+        $this->load->view("registration_view");
+    }
+
+    public function registration_submit() {
+        session_start();
+        echo "inside register submit";
+        $name = $this->input->post('name');
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+        $username = $this->input->post('username');
+        $phonenumber = $this->input->post('phonenumber');
+        //echo "$name";
+        $this->load->model('users_model');
+        $this->users_model->register_insert($username, $email, $password, $name, $phonenumber);
+        $url = "home/login";
+        header("Location:$url");
+    }
+
+    public function user_homepage() {
+        $this->load->view("home_view");
+    }
+
+    public function display_queries_and_comments() {
         $this->load->model('qac_model');
         $course_id = $this->input->post('course_id');
         //$_SESSION['course_id']=$course_id;
-        $result=$this->qac_model->get_queries_and_comments($course_id);
-        $data=array('result'=>$result);
-        $this->load->view('display_qac_view',$data);
-         
-     }
-     
-     public function insert_queries_and_comments()
-     {
-         $this->load->model('qac_model');
-         $result=$this->qac_model->put_queries_and_comments($_SESSION['userid'],$this->input->post('comments'),$_SESSION['course_id']);
-          $url="home/display_queries_and_comments";
-         header("Location:$url");
-         
-         
-     }
-     
-     public function queries()
-     {
-         $this->load->view('queries_view');
-     }
+        $search_data['list'] = $this->qac_model->get_queries_and_comments($course_id);
+        //$data=array('result'=>$result);
+        //$this->load->view('display_qac_view',$data);
+        //$user = $this->user_model->get_user($user_id);
+        //$name = $user->name;
+        //$user_data = array(
+        //    'comments' => $comments,
+        //);
+        //$this->load->view('navbar', $user_data);
+        $this->load->view("display_qac_view", $search_data);
+    }
+
+    public function insert_queries_and_comments() {
+        $this->load->model('qac_model');
+        $result = $this->qac_model->put_queries_and_comments($_SESSION['userid'], $this->input->post('comments'), $_SESSION['course_id']);
+        $url = "home/display_queries_and_comments";
+        header("Location:$url");
+    }
+
+    public function queries() {
+        $this->load->view('queries_view');
+    }
+
 }
 
--- application/models/qac_model.php
+++ application/models/qac_model.php
@@ -6,20 +6,26 @@ class qac_model extends CI_Model {
         parent::__construct();
     }
 
-    function get_queries_and_comments($course_id) {
-        $sql = "select * from qac where course_id=$course_id";
-        $query = $this->db->query($sql);
-        if ($query->num_rows() == 0) {
-            return 0;
-        } else
-            return $query->result_array();
-            
-    }
+    
     function put_queries_and_comments($user_id,$comments,$course_id) {
         $sql = "insert into qac(user_id,comments,course_id) values($user_id,'$comments',$course_id)";
         $query = $this->db->query($sql);
         
             
+    }
+    
+    public function get_queries_and_comments($course_id) {
+
+        $sql = "select * from qac where course_id=$course_id";
+        $query = $this->db->query("$sql");
+        if ($query->num_rows() > 0) {
+            foreach ($query->result_array() as $row) {
+                $data[] = $row;
+            }
+            return $data;
+        } else {
+            return false;
+        }
     }
 
 }
