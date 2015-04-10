6481423f5c261038b58c905bcefefbf69a0b8f1d
madhumita <madhumitagopal9@gmail.com>
2015-04-10 23:09:55 +0530

    pushing changes after major linking.Everyone please check the stuff_required file for their respective completion tasks

--- application/views/os1.php
+++
@@ -1,8 +0,0 @@
-<html>
-    <head>
-        os1
-    </head>
-    <body>
-        <h1>this is os1  </h1>
-    </body>
-</html>
--- application/views/os2.php
+++
@@ -1,8 +0,0 @@
-<html>
-    <head>
-        os2
-    </head>
-    <body>
-        this is os2
-    </body>
-</html>
